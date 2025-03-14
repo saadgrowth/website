c5a87a65b1e66c71c4e52a9a4d54695f7d30ad0c
unknown <saadgrowthops@outlook.com>
2025-03-14 20:24:00 +0000

    Revert

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,32 +12,36 @@
         @endif
     </head>
     <body class="font-sans antialiased">
-        
-        <section>
-            <!-- Container -->
-            <div class="mx-auto w-full max-w-7xl px-5 py-16 md:px-10 md:py-20">
-            <!-- Component -->
-            <div class="grid items-center gap-8 sm:gap-20 md:grid-cols-2">
-                <div class="max-w-2xl">
-                <h2 class="mb-4 text-3xl font-bold md:text-5xl md:leading-snug">
-                    Saad Growth
-                </h2>
-                <p class="mb-6 max-w-lg text-sm text-gray-500 sm:text-base md:mb-12">
-                    Lorem ipsum delor
-                </p>
-                <!-- Form -->
-                <form name="email-form" method="get" class="flex relative w-full max-w-lg border border-solid border-black p-1 focus-within:outline focus-within:outline-2">
-                    <input type="text" class="flex-1 h-9 w-auto bg-white px-3 py-6 text-sm text-black focus:outline-none" placeholder="Enter your name" required="" />
-                    <input type="email" class="flex-1 h-9 w-auto bg-white px-3 py-6 text-sm text-black focus:outline-none" placeholder="Enter your email" required="" />
-                    <input type="submit" value="Subscribe" class="relative w-auto cursor-pointer bg-black px-6 py-2 text-center font-semibold text-white" />
-                </form>
-                </div>
-                <div>
-                <img src="https://firebasestorage.googleapis.com/v0/b/flowspark-1f3e0.appspot.com/o/Tailspark%20Images%2FPlaceholder%20Image.svg?alt=media&token=375a1ea3-a8b6-4d63-b975-aac8d0174074" alt="" class="mx-auto inline-block h-full w-full max-w-xl" />
-                </div>
+        <section class="px-4 py-24 mx-auto max-w-7xl">
+            <div class="w-full mx-auto text-left md:w-11/12 xl:w-9/12 md:text-center">
+              <h1 class="mb-6 text-4xl font-extrabold leading-none tracking-normal text-gray-900 md:text-6xl md:tracking-tight">
+                All your <span class="block w-full text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-purple-500 lg:inline">customer feedback</span> in one single place.
+              </h1>
+              <p class="px-0 mb-6 text-lg text-gray-600 md:text-xl lg:px-24">
+                Hellonext is a feature voting software where you can allow your users to vote on features, publish roadmap, and complete your customer feedback loop.
+              </p>
+              <div class="mb-4 space-x-0 md:space-x-2 md:mb-8">
+                <a class="inline-flex items-center justify-center w-full mb-2 btn btn-primary btn-lg sm:w-auto sm:mb-0" href="#">
+                  Get Started
+                  <svg class="w-4 h-4 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd" />
+                  </svg>
+                </a>
+                <a class="inline-flex items-center justify-center w-full mb-2 btn btn-light btn-lg sm:w-auto sm:mb-0" href="#">
+                  Book a Demo
+                  <svg class="w-4 h-4 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                    <path
+                      fill-rule="evenodd"
+                      d="M6.672 1.911a1 1 0 10-1.932.518l.259.966a1 1 0 001.932-.518l-.26-.966zM2.429 4.74a1 1 0 10-.517 1.932l.966.259a1 1 0 00.517-1.932l-.966-.26zm8.814-.569a1 1 0 00-1.415-1.414l-.707.707a1 1 0 101.415 1.415l.707-.708zm-7.071 7.072l.707-.707A1 1 0 003.465 9.12l-.708.707a1 1 0 001.415 1.415zm3.2-5.171a1 1 0 00-1.3 1.3l4 10a1 1 0 001.823.075l1.38-2.759 3.018 3.02a1 1 0 001.414-1.415l-3.019-3.02 2.76-1.379a1 1 0 00-.076-1.822l-10-4z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </a>
+              </div>
             </div>
+            <div class="w-full mx-auto mt-20 text-center md:w-10/12">
+              <img src="/hero.jpg" alt="Hellonext feedback boards software screenshot" class="w-full rounded-lg shadow-2xl" />
             </div>
-        </section>
-   
+          </section>   
     </body>
 </html>

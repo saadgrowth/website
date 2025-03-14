25016e34d4056ddaa02d1efa591f16e1c08ceeea
unknown <saadgrowthops@outlook.com>
2025-03-14 20:00:38 +0000

    TYes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -58,7 +58,7 @@
                         @endif
                     </header>
 
-                    @livewire('index-component')
+                    <livewire:index-component />
 
                     <footer class="py-16 text-center text-sm text-black dark:text-white/70">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})

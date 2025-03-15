70cab10b198781d81a4ef74bef15f1e67b5dd96b
illushun <admin@conflictcloud.com>
2025-03-15 15:25:36 +0000

    Added header

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,6 +12,66 @@
         @endif
     </head>
     <body class="font-sans antialiased">
+
+    <header class="z-30 w-full px-2 py-4 bg-white sm:px-4">
+      <div class="flex items-center justify-between mx-auto max-w-7xl">
+        <a href="/" title="Kutty Home Page" class="flex items-center">
+          <span class="sr-only">SaadGrowth</span>
+        </a>
+        <div class="flex items-center space-x-1">
+          <div class="hidden space-x-1 md:inline-flex">
+            <a href="#" class="btn btn-sm btn-link">Features</a>
+            <a href="#" class="btn btn-sm btn-link">Sign in</a>
+          </div>
+          <a href="#" class="btn btn-sm btn-primary">Get Started</a>
+          <div class="inline-flex md:hidden" x-data="{ open: false }">
+            <button class="flex-none px-2 btn btn-link btn-sm" @click="open = true">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                aria-hidden="true"
+                class="w-5 h-5"
+              >
+                <line x1="3" y1="12" x2="21" y2="12"></line>
+                <line x1="3" y1="6" x2="21" y2="6"></line>
+                <line x1="3" y1="18" x2="21" y2="18"></line>
+              </svg>
+              <span class="sr-only">Open Menu</span>
+            </button>
+            <div class="absolute top-0 left-0 right-0 z-50 flex flex-col p-2 pb-4 m-2 space-y-3 bg-white rounded shadow" x-show.transition="open" @click.away="open = false" x-cloak>
+              <button class="self-end flex-none px-2 ml-2 btn btn-link btn-icon" @click="open = false">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  aria-hidden="true"
+                >
+                  <line x1="18" y1="6" x2="6" y2="18"></line>
+                  <line x1="6" y1="6" x2="18" y2="18"></line>
+                </svg>
+                <span class="sr-only">Close Menu</span>
+              </button>
+              <a href="#" class="w-full btn btn-link">Features</a>
+              <a href="#" class="w-full btn btn-link">Sign in</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+<!-- Remove the div below -->
+<div class="py-32 bg-gray-400"></div>
         <section class="px-4 py-24 mx-auto max-w-7xl">
             <div class="w-full mx-auto text-left md:w-11/12 xl:w-9/12 md:text-center">
               <h1 class="mb-6 text-4xl font-extrabold leading-none tracking-normal text-gray-900 md:text-6xl md:tracking-tight">

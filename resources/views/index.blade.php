f5f1adf5dca701e6a521b2852eee5c6cfc3c6e66
illushun <admin@conflictcloud.com>
2025-03-16 12:52:31 +0000

    Test

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -111,5 +111,40 @@
         </div>
       </div>
     </header>
+
+<footer class="block">
+  <!-- Container -->
+  <div class="py-16 md:py-20 mx-auto w-full max-w-7xl px-5 md:px-10">
+    <!-- Component -->
+    <div class="flex-col flex items-center">
+      <a href="#" class="mb-8 inline-block max-w-full text-black">
+        <img src="https://assets.website-files.com/6458c625291a94a195e6cf3a/6458c625291a94d6f4e6cf96_Group%2047874-3.png" alt="" class="inline-block max-h-10" />
+      </a>
+      <div class="text-center font-semibold">
+        <a href="#" class="inline-block px-6 py-2 font-normal text-black transition hover:text-blue-600"> About </a>
+        <a href="#" class="inline-block px-6 py-2 font-normal text-black transition hover:text-blue-600"> Features </a>
+        <a href="#" class="inline-block px-6 py-2 font-normal text-black transition hover:text-blue-600"> Works </a>
+        <a href="#" class="inline-block px-6 py-2 font-normal text-black transition hover:text-blue-600"> Support </a>
+        <a href="#" class="inline-block px-6 py-2 font-normal text-black transition hover:text-blue-600"> Help </a>
+      </div>
+      <div class="mb-8 mt-8 border-b border-gray-300 w-48"></div>
+      <div class="mb-12 grid-cols-4 grid-flow-col grid w-full max-w-52 gap-3">
+        <a href="#" class="mx-auto flex-col flex max-w-6 items-center justify-center text-black">
+          <img src="https://assets.website-files.com/6458c625291a94a195e6cf3a/6458c625291a945b4ae6cf7b_Vector-1.svg" alt="" class="inline-block" />
+        </a>
+        <a href="#" class="mx-auto flex-col flex max-w-6 items-center justify-center text-black">
+          <img src="https://assets.website-files.com/6458c625291a94a195e6cf3a/6458c625291a945560e6cf77_Vector.svg" alt="" class="inline-block" />
+        </a>
+        <a href="#" class="mx-auto flex-col flex max-w-6 items-center justify-center text-black">
+          <img src="https://assets.website-files.com/6458c625291a94a195e6cf3a/6458c625291a940535e6cf7a_Vector-3.svg" alt="" class="inline-block" />
+        </a>
+        <a href="#" class="mx-auto flex-col flex max-w-6 items-center justify-center text-black">
+          <img src="https://assets.website-files.com/6458c625291a94a195e6cf3a/6458c625291a9433a9e6cf88_Vector-2.svg" alt="" class="inline-block" />
+        </a>
+      </div>
+      <p class="text-sm sm:text-base"> © Copyright 2024. All rights reserved. </p>
+    </div>
+  </div>
+</footer>
     </body>
 </html>
